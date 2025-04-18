4377074b42cfe2cb02e04dc529c68c4552133b66
KarmoussaCode <Karmoussacode@gmail.com>
2025-04-18 10:59:23 +0100

    vite update
    git hub doas not recognize vite apearently

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mylinktree/', // Set this to your repository name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
+
